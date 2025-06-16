879cbee10b29b8a1a45f46a6fc939124342ef6cc
vamsi <viz.ply@gmail.com>
2025-06-15 21:15:59 -0500

    see if husky runs and cleans pwds r3

--- scripts/replace-org.ps1
+++ scripts/replace-org.ps1
@@ -2,7 +2,7 @@
 
 # Replace informatica.username and informatica.password with fixed values in all .properties files
 Get-ChildItem -Path . -Recurse -Include *.properties | ForEach-Object {
-    (Get-Content $_.FullName) -replace 'informatica\.username\s*=.*', 'informatica.username=user123' | \
-        ForEach-Object { $_ -replace 'informatica\.password\s*=.*', 'informatica.password=pwd123' } | \
+    (Get-Content $_.FullName) -replace 'informatica\.username\s*=.*', 'informatica.username=user123' |
+        ForEach-Object { $_ -replace 'informatica\.password\s*=.*', 'informatica.password=pwd123' } |
         Set-Content $_.FullName
 }
